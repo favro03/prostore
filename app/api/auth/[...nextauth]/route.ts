e908fea7f466c8138dc1fbf0009da383fa79273f
tfavro <wetr9902@gmail.com>
2025-10-23 14:48:27 -0500

    added signIn

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+//the auth.ts file has the config set up
+
+import { handlers } from '@/auth';
+
+export const { GET, POST } = handlers;
---
+++ components/forms/credentials-signin-form.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { useSearchParams } from "next/navigation";
+
+
+const CredentialsSigniInForm = () => {
+    const [data, action] = useActionState(signInWithCredentials, {
+        success: false,
+        message: ''
+    });
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+    const SignInButton = () => {
+        const { pending } = useFormStatus();
+        return (
+            <Button disabled={pending} className="w-full mt-6" variant='default'>
+                {pending ? 'Signing In...' : 'Sign In'}
+            </Button>
+        )
+    }
+    return ( 
+        <form action={action} >
+            <input type="hidden" name="callbackUrl" value={callbackUrl} />
+            <div className="space-y-6">
+                <Label htmlFor='email'>Email</Label>
+                <Input id='email' name='email' type='email'required autoComplete="email" defaultValue={signInDefaultValues.email} />
+                <Label htmlFor='password'>Password</Label>
+                 <Input id='password' name='password' type='password'required autoComplete="password" defaultValue={signInDefaultValues.password} />
+            </div>
+            <div>
+                <SignInButton />
+            </div>
+            {data && !data.success && (
+                <div className="text-center text-destructive">
+                    {data.message}
+                </div>
+            )}
+            <div className="text-sm text-center text-muted-foreground mt-4">
+                Don&apos;t have an account? {' ' }
+                <Link href='/sign-up' target="_self" className='link'>Sign Up</Link>
+                
+            </div>
+            
+        </form>
+
+     );
+}
+ 
+export default CredentialsSigniInForm;
